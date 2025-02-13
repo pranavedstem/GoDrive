be5d136b3048d99e3433eee0368b5c541603dcf1
pranavedstem <pranav.pj@edstem.com>
2025-02-13 16:47:53 +0530

    push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+import 'package:dummyprojecr/services/notification.dart';
 import 'package:dummyprojecr/view/screens/splash.dart';
 import 'package:dummyprojecr/view/widgets/theme.dart';
 import 'package:dummyprojecr/viewModel/home_view_model.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-void main() {
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("🔔 Background Message: ${message.notification?.title}");
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseNotification().initNotifications();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(
     MultiProvider(
       providers: [
